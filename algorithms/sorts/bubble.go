cdfbd24f93887f28841a8261a2d2728ee1d54fbf
mah <zanzinzzo@gmail.com>
2024-01-27 16:39:14 -0300

    feat: add bubbleSort

---
+++ algorithms/sorts/bubble.go
@@ -0,0 +1,17 @@
+package sorts
+
+func BubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		swapped := false
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j+1] < arr[j] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
+		}
+		if swapped == false {
+			return arr
+		}
+	}
+	return arr
+}
